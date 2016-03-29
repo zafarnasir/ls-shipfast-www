0072f7feb5ecd3f8aeb5963bb8facae4ea36892f
Abhinav Pobbati <apobbati@codeblock.io>
2016-03-28 23:27:38 -0400

    Add dummy unit and integration tests

---
+++ src/intTest/java/com/shipfast/test/integration/ApplicationTest.java
@@ -0,0 +1,21 @@
+package com.shipfast.test.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ApplicationTest {
+    @Test
+    public void shouldPass() {
+        assertThat(true).isEqualTo(true);
+    }
+}
